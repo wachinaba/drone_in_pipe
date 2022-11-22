77bb3566966b92fd81d428a2bc1ae1cfa918a513
wachinaba <wachinaba@gmail.com>
2022-11-22 21:31:44 +0900

    fix 型名
    コントローラ作成 #2

--- drone_controller_io/src/rc_io_normalizer.py
+++ drone_controller_io/src/rc_io_normalizer.py
@@ -1,6 +1,7 @@
-import rospy
 from rospy import Subscriber, Publisher
 from mavros_msgs.msg import RCIn, OverrideRCIn
+from drone_controller_io.msg import NormalizedRCIn, NormalizedRCOut
+
 
 class RCNormalizerNode:
     def __init__(self) -> None:
@@ -8,10 +9,11 @@ class RCNormalizerNode:
         self.normalized_output = NormalizedRCOut
 
         self.raw_input_subscriber = Subscriber("/mavros/rc/in", RCIn, self.raw_input_cb)
-        self.normalized_input_publisher = Publisher("/normalized_controller/in", queue_size=5)
-        self.normalized_output_subscriber = Subscriber("/normalized_controller/out")
+        self.normalized_input_publisher = Publisher("/normalized_controller/in", NormalizedRCIn, queue_size=5)
+        self.normalized_output_subscriber = Subscriber(
+            "/normalized_controller/out", NormalizedRCOut, self.normalized_out_cb
+        )
         self.raw_override_publisher = Publisher("/mavros/rc/override", OverrideRCIn, queue_size=5)
 
     def raw_input_cb(self, msg: RCIn):
         pass
-
