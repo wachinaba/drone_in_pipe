8d684a48f96ac39ddc121455cd084cec6eb01158
wachinaba <wachinaba@gmail.com>
2022-12-14 06:08:40 +0900

    add auto alt feature
    簡易的な自動制御パッケージの作成 #3

--- instant_autoflight/src/rc_to_attitude.py
+++ instant_autoflight/src/rc_to_attitude.py
@@ -20,16 +20,17 @@ class RCToAttitudeNode:
         self.current_orientaion = Rotation.from_matrix(np.eye(3))
         self.initial_yaw = None
 
-        self.current_altitude = 0.0
         self.target_altitude = 1.0
+        self.previous_altitude = 0.0
 
         self.normalized_control_in = [ChannelState() for i in range(16)]
 
         self.rc_subscriber = Subscriber("/normalized_rc/in", NormalizedRCIn, self.rc_cb, queue_size=1)
-        self.rangefinder_subscriber = Subscriber(
-            "/mavros/distance_sensor/rangefinder_pub", Range, self.rangefinder_cb, queue_size=1
-        )
         self.imu_subscriber = Subscriber("/mavros/imu/data", Imu, self.imu_cb, queue_size=1)
+
+        self.rangefinder_subscriber = Subscriber(
+            "/mavros/distance_sensor/rangefinder_sub", Range, self.rangefinder_cb, queue_size=1
+        )
 
         self.attitude_publisher = Publisher("/mavros/setpoint_raw/attitude", AttitudeTarget, queue_size=5)
 
@@ -44,7 +45,7 @@ class RCToAttitudeNode:
         target_attitude.type_mask = 1 & 2 & 4
 
         pose_quat = Rotation.from_euler(
-            "ZYX", np.array([self.initial_yaw + self.yaw, self.thrust_x * -20, self.thrust_y * 20]), True
+            "ZYX", np.array([self.initial_yaw + self.yaw, self.thrust_x * 20, self.thrust_y * -20]), True
         ).as_quat()
         target_attitude.orientation.w = pose_quat[3]
         target_attitude.orientation.x = pose_quat[0]
@@ -56,6 +57,18 @@ class RCToAttitudeNode:
 
         self.attitude_publisher.publish(target_attitude)
 
+    def rangefinder_cb(self, msg: Range):
+        error = self.target_altitude - msg.range
+        if msg.range > 0.2:
+            self.thrust_z = min(error / 3.0 + 0.5 - (msg.range - self.previous_altitude) / 0.2, 0.7)
+        else:
+            self.thrust_z = min(error + 0.5 - (msg.range - self.previous_altitude) / 0.1, 1.0)
+
+        if abs(self.normalized_control_in[2].value) > 0.1:
+            self.thrust_z = (self.normalized_control_in[2].value + 1.0) / 2.0
+
+        self.previous_altitude = msg.range
+
     def imu_cb(self, msg: Imu) -> None:
         self.current_orientaion = Rotation(
             np.array([msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w])
@@ -66,7 +79,7 @@ class RCToAttitudeNode:
     def set_thrust(self):
         self.thrust_x = self.normalized_control_in[1].value
         self.thrust_y = self.normalized_control_in[3].value
-        self.yaw += self.normalized_control_in[0].value * -1
+        self.yaw += self.normalized_control_in[0].value
 
     def rc_cb(self, msg: NormalizedRCIn) -> None:
         self.normalized_control_in = msg.channel_states
