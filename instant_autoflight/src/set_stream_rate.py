bd55c19ddeaf3e88da1083f7ed15af32795780ef
wachinaba <wachinaba@gmail.com>
2022-12-15 14:19:20 +0900

    add setter of stream_rate node
    簡易的な自動制御パッケージの作成 #3

---
+++ instant_autoflight/src/set_stream_rate.py
@@ -0,0 +1,25 @@
+import rospy
+from rospy import ServiceProxy, ServiceException, Rate
+
+from mavros_msgs.srv import StreamRate
+
+
+def main():
+    rospy.init_node("stream_rate_publisher")
+
+    stream_rate = int(rospy.get_param("~stream_rate", 60))
+    update_rate = int(rospy.get_param("~update_rate", 10))
+    rate = Rate(update_rate)
+
+    command = ServiceProxy("/mavros/set_stream_rate")
+
+    while not rospy.is_shutdown():
+        try:
+            command(StreamRate(0, stream_rate, True))
+        except ServiceException as e:
+            rospy.logerr(f"Service call failed: {e}")
+        rate.sleep()
+
+
+if __name__ == "__main__":
+    main()
