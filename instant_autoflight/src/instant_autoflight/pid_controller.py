12a8eff1f67bec05b2927e6d643738d30c4ba8e1
wachinaba <wachinaba@gmail.com>
2022-12-18 12:21:11 +0900

    add pid controller
    簡易的な自動制御パッケージの作成 #3

---
+++ instant_autoflight/src/instant_autoflight/pid_controller.py
@@ -0,0 +1,36 @@
+class LPF:
+    def __init__(self, blend) -> None:
+        self.prev_output = 0.0
+        self.blend = blend
+
+    def update(self, input) -> float:
+        output = input * self.blend + self.prev_output * (1 - self.blend)
+        self.prev_output = output
+        return output
+
+
+class PIDController:
+    def __init__(self, p, i, d, constant, filter) -> None:
+        self.p = p
+        self.i = i
+        self.d = d
+        self.constant = constant
+        self.filter = filter
+        self.prev_input = 0.0
+        self.prev_error = 0.0
+        self.i_output = 0.0
+        self.p_output = 0.0
+        self.d_output = 0.0
+        self.output = 0.0
+
+    def update(self, input, target) -> float:
+        error = target - input
+        self.i_output += error * self.i
+        self.p_output = error * self.p
+        self.d_output = self.filter.update(self.prev_input - input) * self.d
+
+        self.output = self.p_output + self.i_output + self.d_output + self.constant
+        self.prev_error = error
+        self.prev_input = input
+
+        return self.output
