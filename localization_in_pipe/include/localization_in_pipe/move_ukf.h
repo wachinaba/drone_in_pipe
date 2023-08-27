e79481a18380e384a947238b5fa3b4a78370414b
wachinaba <wachinaba@gmail.com>
2023-08-27 17:58:05 +0900

    change library to implement ukf

--- localization_in_pipe/include/localization_in_pipe/move_ukf.h
+++ localization_in_pipe/include/localization_in_pipe/move_ukf.h
@@ -1,49 +1,186 @@
-#ifndef MOSE_EKF_H
-#define MOSE_EKF_H
+#ifndef MOVE_EKF_H
+#define MOVE_EKF_H
 
 #include <ros/ros.h>
 #include <Eigen/Dense>
 
-#include <ukf_ros/core.h>
-#include <ukf_ros/state_vector.h>
-#include <ukf_ros/measurement_vector.h>
-#include <ukf_ros/integrator.h>
-#include <ukf_ros/core.h>
+#include <kalman/SystemModel.hpp>
+#include <kalman/UnscentedKalmanFilter.hpp>
 
-enum StateFields
+namespace MoveUKF
 {
-  Velocity
+template <typename T>
+class State : public Kalman::Vector<T, 3>
+{
+public:
+  KALMAN_VECTOR(State, T, 3)
+
+  //! Velocity in x-direction
+  static constexpr size_t VEL_X = 0;
+  //! Velocity in y-direction
+  static constexpr size_t VEL_Y = 1;
+  //! Velocity in z-direction
+  static constexpr size_t VEL_Z = 2;
+
+  T vel_x() const
+  {
+    return (*this)[VEL_X];
+  }
+  T vel_y() const
+  {
+    return (*this)[VEL_Y];
+  }
+  T vel_z() const
+  {
+    return (*this)[VEL_Z];
+  }
+
+  T& vel_x()
+  {
+    return (*this)[VEL_X];
+  }
+  T& vel_y()
+  {
+    return (*this)[VEL_Y];
+  }
+  T& vel_z()
+  {
+    return (*this)[VEL_Z];
+  }
 };
 
-enum MeasurementFields
+template <typename T>
+class Control : public Kalman::Vector<T, 3>
 {
-  MeasuredVelocity
+public:
+  KALMAN_VECTOR(Control, T, 3)
+
+  //! Acceleration in x-direction
+  static constexpr size_t ACC_X = 0;
+  //! Acceleration in y-direction
+  static constexpr size_t ACC_Y = 1;
+  //! Acceleration in z-direction
+  static constexpr size_t ACC_Z = 2;
+
+  T acc_x() const
+  {
+    return (*this)[ACC_X];
+  }
+  T acc_y() const
+  {
+    return (*this)[ACC_Y];
+  }
+  T acc_z() const
+  {
+    return (*this)[ACC_Z];
+  }
+
+  T& acc_x()
+  {
+    return (*this)[ACC_X];
+  }
+  T& acc_y()
+  {
+    return (*this)[ACC_Y];
+  }
+  T& acc_z()
+  {
+    return (*this)[ACC_Z];
+  }
 };
 
-using MoveStateVector = UKF::StateVector<UKF::Field<Velocity, UKF::Vector<3>>>;
-
-using MoveMeasurementVector = UKF::FixedMeasurementVector<UKF::Field<MeasuredVelocity, UKF::Vector<3>>>;
-
-template <>
-template <>
-MoveStateVector MoveStateVector::derivative<UKF::Vector<3>>(const UKF::Vector<3>& acceleration) const
+template <typename T, template <class> class CovarianceBase = Kalman::StandardBase>
+class SystemModel : public Kalman::SystemModel<State<T>, Control<T>, CovarianceBase>
 {
-  MoveStateVector derivative;
-  derivative.set_field<Velocity>(acceleration);
-  return derivative;
-}
+public:
+  //! State type shortcut definition
+  typedef MoveUKF::State<T> S;
+  //! Control type shortcut definition
+  typedef MoveUKF::Control<T> C;
 
-template <>
-template <>
-UKF::Vector<3>
-MoveMeasurementVector::expected_measurement<MoveStateVector, Velocity, UKF::Vector<3>, UKF::Vector<3>, UKF::Vector<3>>(
-    const MoveStateVector& state, const UKF::Vector<3>& angular_velocity, const UKF::Vector<3>& range_vector,
-    const UKF::Vector<3>& camera_z_axis)
+  S f(const S& x, const C& u) const override
+  {
+    S x_;
+    x_.vel_x() = x.vel_x() + u.acc_x();
+    x_.vel_y() = x.vel_y() + u.acc_y();
+    x_.vel_z() = x.vel_z() + u.acc_z();
+    return x_;
+  }
+};
+
+template <typename T>
+class Measurement : public Kalman::Vector<T, 3>
 {
-  auto velocity_3d = angular_velocity.cross(range_vector) + state.get_field<Velocity>();
-  return velocity_3d - velocity_3d.dot(camera_z_axis) * camera_z_axis;
-}
+public:
+  KALMAN_VECTOR(Measurement, T, 3)
 
-using MoveUKFCore = UKF::Core<MoveStateVector, MoveMeasurementVector, UKF::IntegratorRK4>;
+  //! Measured velocity in x-direction
+  static constexpr size_t MEASURED_VEL_X = 0;
+  //! Measured velocity in y-direction
+  static constexpr size_t MEASURED_VEL_Y = 1;
+  //! Measured velocity in z-direction
+  static constexpr size_t MEASURED_VEL_Z = 2;
 
-#endif  // MOSE_EKF_H
+  T measured_vel_x() const
+  {
+    return (*this)[MEASURED_VEL_X];
+  }
+  T measured_vel_y() const
+  {
+    return (*this)[MEASURED_VEL_Y];
+  }
+  T measured_vel_z() const
+  {
+    return (*this)[MEASURED_VEL_Z];
+  }
+
+  T& measured_vel_x()
+  {
+    return (*this)[MEASURED_VEL_X];
+  }
+  T& measured_vel_y()
+  {
+    return (*this)[MEASURED_VEL_Y];
+  }
+  T& measured_vel_z()
+  {
+    return (*this)[MEASURED_VEL_Z];
+  }
+};
+
+template <typename T, template <class> class CovarianceBase = Kalman::StandardBase>
+class MeasurementModel : public Kalman::MeasurementModel<State<T>, Measurement<T>, CovarianceBase>
+{
+public:
+  //! State type shortcut definition
+  typedef MoveUKF::State<T> S;
+
+  //! Measurement type shortcut definition
+  typedef MoveUKF::Measurement<T> M;
+
+  M h(const S& x) const override
+  {
+    M velocity_2d;
+    M velocity_3d = x + angular_velocity_.cross(range_vector_);
+
+    velocity_2d = velocity_3d - velocity_3d.dot(camera_z_axis_) * camera_z_axis_;
+    return velocity_2d;
+  }
+
+  void setSensorStatus(const Kalman::Vector<T, 3>& angular_velocity, const Kalman::Vector<T, 3>& range_vector,
+                       const Kalman::Vector<T, 3>& camera_z_axis)
+  {
+    angular_velocity_ = angular_velocity;
+    range_vector_ = range_vector;
+    camera_z_axis_ = camera_z_axis;
+  }
+
+protected:
+  Kalman::Vector<T, 3> angular_velocity_;
+  Kalman::Vector<T, 3> range_vector_;
+  Kalman::Vector<T, 3> camera_z_axis_;
+};
+
+}  // namespace MoveUKF
+
+#endif  // MOVE_EKF_H
--- localization_in_pipe/src/nodelets/move_ukf_nodelet.cpp
+++ localization_in_pipe/src/nodelets/move_ukf_nodelet.cpp
@@ -5,8 +5,10 @@
 #include <pluginlib/class_list_macros.h>
 
 #include <geometry_msgs/TwistWithCovarianceStamped.h>
+#include <sensor_msgs/Imu.h>
 #include "localization_in_pipe_msgs/IntegratedFlow.h"
 
+#include <kalman/UnscentedKalmanFilter.hpp>
 #include "localization_in_pipe/move_ukf.h"
 
 namespace localization_in_pipe
@@ -16,6 +18,7 @@ class move_ukf_nodelet : public nodelet::Nodelet
 public:
   virtual void onInit();
   void integratedFlowCallback(const localization_in_pipe_msgs::IntegratedFlow::ConstPtr& msg);
+  void imuCallback(const sensor_msgs::Imu::ConstPtr& msg);
 
 private:
   ros::NodeHandle nh_;
@@ -23,11 +26,25 @@ private:
 
   ros::Publisher velocity_pub_;
   ros::Subscriber integrated_flow_sub_;
+  ros::Subscriber imu_sub_;
 
-  MoveUKFCore move_ukf_;
+  typedef float T;
+  MoveUKF::State<T> filter_state_;
+  MoveUKF::Control<T> filter_control_;
+  MoveUKF::SystemModel<T> filter_system_model_;
+  MoveUKF::MeasurementModel<T> filter_measurement_model_;
+
+  MoveUKF::Measurement<T> filter_measurement_;
+  Eigen::Matrix<T, 3, 3> filter_covariance_matrix_;
+  Eigen::Matrix<T, 3, 1> filter_angular_velocity_;
+  Eigen::Matrix<T, 3, 1> filter_range_vector_;
+  Eigen::Matrix<T, 3, 1> filter_camera_z_axis_;
+
+  Kalman::UnscentedKalmanFilter<MoveUKF::State<T>> filter_;
 
   ros::Time prev_stamp_;
   geometry_msgs::TwistWithCovarianceStamped velocity_msg;
+  sensor_msgs::Imu::ConstPtr imu_msg_;
 
   void publishVelocity(const localization_in_pipe_msgs::IntegratedFlow::ConstPtr& msg);
 };
@@ -39,17 +56,27 @@ void move_ukf_nodelet::onInit()
   nh_ = getNodeHandle();
   pnh_ = getPrivateNodeHandle();
 
-  // initialize move_ukf
-  move_ukf_.state.set_field<Velocity>(UKF::Vector<3>::Zero());
-  move_ukf_.covariance = MoveStateVector::CovarianceMatrix::Identity();
+  prev_stamp_ = ros::Time(0);
 
-  move_ukf_.process_noise_covariance = MoveStateVector::CovarianceMatrix::Identity() * 1e-5;
-  move_ukf_.measurement_covariance = MoveMeasurementVector::CovarianceMatrix::Identity() * 1e-5;
+  // initialize filter
+  filter_state_ = MoveUKF::State<T>::Zero();
+  filter_control_ = MoveUKF::Control<T>::Zero();
+
+  filter_system_model_ = MoveUKF::SystemModel<T>();
+  filter_system_model_.setCovariance(Eigen::Matrix<T, 3, 3>::Identity() * 1e-5);  // process noise covariance
+
+  filter_measurement_model_ = MoveUKF::MeasurementModel<T>();
+  filter_measurement_model_.setCovariance(Eigen::Matrix<T, 3, 3>::Identity() * 1e-3);  // measurement covariance
+
+  filter_ = Kalman::UnscentedKalmanFilter<MoveUKF::State<T>>(0.001, 2.0, 0.0);
+  filter_.init(filter_state_);
+  filter_.setCovariance(Eigen::Matrix<T, 3, 3>::Identity() * 1.0);
 
   velocity_msg = geometry_msgs::TwistWithCovarianceStamped();
 
   velocity_pub_ = pnh_.advertise<geometry_msgs::TwistWithCovarianceStamped>("velocity", 1);
   integrated_flow_sub_ = pnh_.subscribe("integrated_flow", 10, &move_ukf_nodelet::integratedFlowCallback, this);
+  imu_sub_ = pnh_.subscribe("imu", 10, &move_ukf_nodelet::imuCallback, this);
 }
 
 void move_ukf_nodelet::integratedFlowCallback(const localization_in_pipe_msgs::IntegratedFlow::ConstPtr& msg)
@@ -70,37 +97,55 @@ void move_ukf_nodelet::integratedFlowCallback(const localization_in_pipe_msgs::I
     return;
   }
 
-  MoveMeasurementVector measurement;
-  measurement.set_field<MeasuredVelocity>(
-      UKF::Vector<3>(msg->measured_velocity.x, msg->measured_velocity.y, msg->measured_velocity.z));
+  filter_control_.acc_x() = imu_msg_->linear_acceleration.x * dt;
+  filter_control_.acc_y() = imu_msg_->linear_acceleration.y * dt;
+  filter_control_.acc_z() = imu_msg_->linear_acceleration.z * dt;
+
+  filter_state_ = filter_.predict(filter_system_model_, filter_control_);
 
   // set covariance
   for (int i = 0; i < 3; i++)
   {
     for (int j = 0; j < 3 - i; j++)
     {
-      move_ukf_.measurement_covariance(i, j) = msg->covariance[3 * i + j];
+      filter_covariance_matrix_(i, j) = filter_covariance_matrix_(j, i) = filter_covariance_matrix_(i, j);
     }
   }
 
-  move_ukf_.step(dt, measurement);
+  // set measurement
+  filter_measurement_model_.setCovariance(filter_covariance_matrix_);
+  filter_angular_velocity_ << imu_msg_->angular_velocity.x, imu_msg_->angular_velocity.y, imu_msg_->angular_velocity.z;
+  filter_range_vector_ << msg->range.x, msg->range.y, msg->range.z;
+  filter_camera_z_axis_ << msg->camera_z_axis.x, msg->camera_z_axis.y, msg->camera_z_axis.z;
+
+  filter_measurement_model_.setSensorStatus(filter_angular_velocity_, filter_range_vector_, filter_camera_z_axis_);
+
+  filter_measurement_ << msg->measured_velocity.x, msg->measured_velocity.y, msg->measured_velocity.z;
+
+  // update
+  filter_state_ = filter_.update(filter_measurement_model_, filter_measurement_);
 
   publishVelocity(msg);
+}
+
+void move_ukf_nodelet::imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
+{
+  imu_msg_ = msg;
 }
 
 void move_ukf_nodelet::publishVelocity(const localization_in_pipe_msgs::IntegratedFlow::ConstPtr& msg)
 {
   velocity_msg.header.stamp = msg->header.stamp;
   velocity_msg.header.frame_id = msg->header.frame_id;
-  velocity_msg.twist.twist.linear.x = move_ukf_.state.get_field<Velocity>()(0);
-  velocity_msg.twist.twist.linear.y = move_ukf_.state.get_field<Velocity>()(1);
-  velocity_msg.twist.twist.linear.z = move_ukf_.state.get_field<Velocity>()(2);
+  velocity_msg.twist.twist.linear.x = filter_state_.vel_x();
+  velocity_msg.twist.twist.linear.y = filter_state_.vel_y();
+  velocity_msg.twist.twist.linear.z = filter_state_.vel_z();
 
   for (int i = 0; i < 3; i++)
   {
     for (int j = 0; j < 3; j++)
     {
-      velocity_msg.twist.covariance[6 * i + j] = move_ukf_.covariance(i, j);
+      velocity_msg.twist.covariance[6 * i + j] = filter_.getCovariance()(i, j);
     }
   }
 
