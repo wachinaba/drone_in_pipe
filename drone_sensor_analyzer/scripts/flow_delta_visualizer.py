b82cba45ca9627473c6f372bd90344ad94754f91
wachinaba <wachinaba@gmail.com>
2023-10-08 01:19:34 +0900

    add visualizers

---
+++ drone_sensor_analyzer/scripts/flow_delta_visualizer.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
+
+from optical_flow_msgs.msg import OpticalFlowDelta
+
+
+class OpticalFlowDeltaVisualizer:
+    def __init__(self):
+        # ROSパラメータの読み込み
+        self.frame_id = rospy.get_param("~frame_id", "")
+        self.marker_id = rospy.get_param("~marker_id", 0)
+        self.arrow_shaft_diameter = rospy.get_param("~arrow_shaft_diameter", 0.02)
+        self.arrow_head_diameter = rospy.get_param("~arrow_head_diameter", 0.04)
+        self.arrow_head_length = rospy.get_param("~arrow_head_length", 0.1)
+
+        # color, list of float
+        self.color = rospy.get_param("~color", [1.0, 0.0, 0.0, 1.0])
+
+        self.ns = rospy.get_param("~namespace", "flow_delta")
+        self.delta_scale = rospy.get_param("~delta_scale", 1.0)
+
+        self.pub = rospy.Publisher("marker", Marker, queue_size=10)
+        rospy.Subscriber("flow_delta", OpticalFlowDelta, self.flow_callback)
+
+    def flow_callback(self, msg: OpticalFlowDelta):
+        marker = Marker()
+
+        if self.frame_id == "":
+            marker.header.frame_id = msg.header.frame_id
+        else:
+            marker.header.frame_id = self.frame_id
+
+        marker.header.stamp = msg.header.stamp
+        marker.ns = self.ns
+        marker.id = self.marker_id
+        marker.type = Marker.ARROW
+        marker.action = Marker.ADD
+
+        marker.points.append(Point(0, 0, 0))  # Start point (origin)
+        marker.points.append(
+            Point(msg.delta_px * self.delta_scale, msg.delta_py * self.delta_scale, 0)
+        )
+
+        marker.scale.x = self.arrow_shaft_diameter
+        marker.scale.y = self.arrow_head_diameter
+        marker.scale.z = self.arrow_head_length
+
+        marker.color.r = self.color[0]
+        marker.color.g = self.color[1]
+        marker.color.b = self.color[2]
+        marker.color.a = self.color[3]
+
+        self.pub.publish(marker)
+
+
+if __name__ == "__main__":
+    rospy.init_node("vector_to_arrow_node", anonymous=True)
+    node = OpticalFlowDeltaVisualizer()
+    rospy.spin()
---
+++ drone_sensor_analyzer/scripts/integrated_flow_visualizer.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import ColorRGBA
+from geometry_msgs.msg import Point, Vector3
+from visualization_msgs.msg import Marker, MarkerArray
+
+from localization_in_pipe_msgs.msg import IntegratedFlow
+
+
+class OpticalFlowDeltaVisualizer:
+    def __init__(self):
+        self.marker_id = rospy.get_param("~marker_id", 0)
+        self.arrow_shaft_diameter = rospy.get_param("~arrow_shaft_diameter", 0.02)
+        self.arrow_head_diameter = rospy.get_param("~arrow_head_diameter", 0.04)
+        self.arrow_head_length = rospy.get_param("~arrow_head_length", 0.1)
+        # color, list of float
+        self.color = dict()
+        self.color["velocity"] = rospy.get_param(
+            "~color_velocity", [1.0, 0.0, 0.0, 1.0]
+        )
+        self.color["range"] = rospy.get_param("~color_range", [0.0, 1.0, 0.0, 1.0])
+        self.color["camera"] = rospy.get_param("~color_camera", [0.0, 0.0, 1.0, 1.0])
+        self.ns_prefix = rospy.get_param("~namespace_prefix", "")
+
+        self.pub = rospy.Publisher("marker", MarkerArray, queue_size=10)
+        self.sub = rospy.Subscriber(
+            "integrated_flow", IntegratedFlow, self.flow_callback
+        )
+
+    def flow_callback(self, msg: IntegratedFlow):
+        vectors = {
+            "velocity": Point(
+                msg.measured_velocity.x,
+                msg.measured_velocity.y,
+                msg.measured_velocity.z,
+            ),
+            "range": Point(msg.range.x, msg.range.y, msg.range.z),
+            "camera": Point(
+                msg.camera_z_axis.x, msg.camera_z_axis.y, msg.camera_z_axis.z
+            ),
+        }
+        marker_array = MarkerArray(
+            markers=[
+                Marker(
+                    header=msg.header,
+                    ns=f"{self.ns_prefix}_{kind}",
+                    id=self.marker_id,
+                    type=Marker.ARROW,
+                    action=Marker.ADD,
+                    points=[
+                        Point(0, 0, 0),  # Start point (origin)
+                        vectors[kind],
+                    ],
+                    scale=Vector3(
+                        self.arrow_shaft_diameter,
+                        self.arrow_head_diameter,
+                        self.arrow_head_length,
+                    ),
+                    color=ColorRGBA(
+                        r=self.color[kind][0],
+                        g=self.color[kind][1],
+                        b=self.color[kind][2],
+                        a=self.color[kind][3],
+                    ),
+                )
+                for kind in ["velocity", "range", "camera"]
+            ]
+        )
+
+        self.pub.publish(marker_array)
+
+
+if __name__ == "__main__":
+    rospy.init_node("vector_to_arrow_node", anonymous=True)
+    node = OpticalFlowDeltaVisualizer()
+    rospy.spin()
